70cf2f33a0ec37a336fd7d52aec85d0f9d77cab9
Orlin Dimitrov <orlin369@gmail.com>
2023-02-16 00:08:48 +0200

    Add some more code.

--- firmware/PS2toXT/ApplicationConfiguration.h
+++ firmware/PS2toXT/ApplicationConfiguration.h
@@ -3,13 +3,55 @@
 
 #pragma region Definitions
 
+/**
+ * @brief If uncomment then no XT data will be send through the interface.
+ * 
+ */
+#define DUMMY_SEND
+
+/**
+ * @brief Send data via serial port what is going on.
+ * 
+ */
 #define DEBUG
 
-#define PIN_PS_CLK 2 // PS CLOCK DATA
-#define PIN_PS_DATA 4 // PS DATA PIN
+/**
+ * @brief XT byte size.
+ * 
+ */
+#define XT_BYTE_SIZE 8
 
-#define PIN_XT_CLK 3 // XT CLOCK PIN
-#define PIN_XT_DATA 5 // XT DATA PIN
+/**
+ * @brief Trans code table size.
+ * 
+ */
+#define TRANS_CODE_TABLE_SIZE 256
+
+/**
+ * @brief PS CLOCK DATA
+ * 
+ */
+#define PIN_PS_CLK 2
+
+/**
+ * @brief PS DATA PIN
+ * 
+ */
+#define PIN_PS_DATA 4
+
+/**
+ * @brief XT CLOCK PIN
+ * 
+ */
+#define PIN_XT_CLK 3
+
+/**
+ * @brief XT DATA PIN
+ * 
+ */
+#define PIN_XT_DATA 5
+
+
 
 #pragma endregion
 
--- firmware/PS2toXT/PS2toXT.ino
+++ firmware/PS2toXT/PS2toXT.ino
@@ -25,7 +25,7 @@ PS2KeyAdvanced PS2Keyboard_g;
  * @brief Trans code table.
  * 
  */
-unsigned char TransCodeTable_g[256];
+unsigned char TransCodeTable_g[TRANS_CODE_TABLE_SIZE];
 
 /**
  * @brief Trans coded key.
@@ -35,175 +35,28 @@ unsigned char TransCoded_g;
 
 #pragma endregion
 
+#pragma region Prototypes
+
 /**
  * @brief Set the up trans table map.
  * 
  */
-void setup_trans_table()
-{
-    for (int index = 0; index < 256; index++)
-    {
-        TransCodeTable_g[index] = 0xff;
-    }
-
-    TransCodeTable_g[PS2_KEY_SCROLL] = 0x46;
-    TransCodeTable_g[PS2_KEY_CAPS] = 0x3A;
-    TransCodeTable_g[PS2_KEY_PAUSE] = 0x5f;
-    TransCodeTable_g[PS2_KEY_L_SHIFT] = 0x2A;
-    TransCodeTable_g[PS2_KEY_R_SHIFT] = 0x36;
-    TransCodeTable_g[PS2_KEY_L_ALT] = 0x38;
-    TransCodeTable_g[PS2_KEY_L_CTRL] = 0x1D;
-    TransCodeTable_g[PS2_KEY_L_ARROW] = 0x4B;
-    TransCodeTable_g[PS2_KEY_R_ARROW] = 0x4D;
-    TransCodeTable_g[PS2_KEY_UP_ARROW] = 0x48;
-    TransCodeTable_g[PS2_KEY_DN_ARROW] = 0x50;
-    TransCodeTable_g[PS2_KEY_DELETE] = 0x53;
-    TransCodeTable_g[PS2_KEY_ESC] = 0x01;
-    TransCodeTable_g[PS2_KEY_BS] = 0x0E;
-    TransCodeTable_g[PS2_KEY_TAB] = 0x0F;
-    TransCodeTable_g[PS2_KEY_ENTER] = 0x1C;
-    TransCodeTable_g[PS2_KEY_SPACE] = 0x39;
-    TransCodeTable_g[PS2_KEY_SEMI] = 0x27;
-    TransCodeTable_g[PS2_KEY_KP0] = 0x52;
-    TransCodeTable_g[PS2_KEY_KP1] = 0x4F;
-    TransCodeTable_g[PS2_KEY_KP2] = 0x50;
-    TransCodeTable_g[PS2_KEY_KP3] = 0x51;
-    TransCodeTable_g[PS2_KEY_KP4] = 0x4B;
-    TransCodeTable_g[PS2_KEY_KP5] = 0x4C;
-    TransCodeTable_g[PS2_KEY_KP6] = 0x4D;
-    TransCodeTable_g[PS2_KEY_KP7] = 0x47;
-    TransCodeTable_g[PS2_KEY_KP8] = 0x48;
-    TransCodeTable_g[PS2_KEY_KP9] = 0x49;
-    TransCodeTable_g[PS2_KEY_OPEN_SQ] = 0x1A;
-    TransCodeTable_g[PS2_KEY_CLOSE_SQ] = 0x1B;
-    TransCodeTable_g[PS2_LOCK_NUM] = 0x45;
-    //TransCodeTable_g[PS2_KEY_KP_DOT]=0x53;
-    TransCodeTable_g[PS2_KEY_KP_PLUS] = 0x4E;
-    TransCodeTable_g[PS2_KEY_KP_MINUS] = 0x4A;
-    TransCodeTable_g[PS2_KEY_KP_TIMES] = 0x37;
-    TransCodeTable_g[PS2_KEY_KP_DIV] = 0x35;
-    TransCodeTable_g[PS2_KEY_KP_ENTER] = 0x01;
-    TransCodeTable_g[PS2_KEY_0] = 0x0B;
-    TransCodeTable_g[PS2_KEY_1] = 0x02;
-    TransCodeTable_g[PS2_KEY_2] = 0x03;
-    TransCodeTable_g[PS2_KEY_3] = 0x04;
-    TransCodeTable_g[PS2_KEY_4] = 0x05;
-    TransCodeTable_g[PS2_KEY_5] = 0x06;
-    TransCodeTable_g[PS2_KEY_6] = 0x07;
-    TransCodeTable_g[PS2_KEY_7] = 0x08;
-    TransCodeTable_g[PS2_KEY_8] = 0x09;
-    TransCodeTable_g[PS2_KEY_9] = 0x0A;
-    TransCodeTable_g[PS2_KEY_APOS] = 0x28;
-    TransCodeTable_g[PS2_KEY_COMMA] = 0x33;
-    TransCodeTable_g[PS2_KEY_MINUS] = 0x0C;
-    TransCodeTable_g[PS2_KEY_SCROLL] = 0x46;
-    TransCodeTable_g[PS2_KEY_DOT] = 0x34;
-    TransCodeTable_g[PS2_KEY_BACK] = 0x2B;
-    TransCodeTable_g[PS2_KEY_SINGLE] = 0x29;
-    TransCodeTable_g[PS2_KEY_DIV] = 0x35;
-    TransCodeTable_g[PS2_KEY_SINGLE] = 28;
-    TransCodeTable_g[PS2_KEY_A] = 0x1E;
-    TransCodeTable_g[PS2_KEY_B] = 0x30;
-    TransCodeTable_g[PS2_KEY_C] = 0x2E;
-    TransCodeTable_g[PS2_KEY_D] = 0x20;
-    TransCodeTable_g[PS2_KEY_E] = 0x12;
-    TransCodeTable_g[PS2_KEY_F] = 0x21;
-    TransCodeTable_g[PS2_KEY_G] = 0x22;
-    TransCodeTable_g[PS2_KEY_H] = 0x23;
-    TransCodeTable_g[PS2_KEY_I] = 0x17;
-    TransCodeTable_g[PS2_KEY_J] = 0x24;
-    TransCodeTable_g[PS2_KEY_K] = 0x25;
-    TransCodeTable_g[PS2_KEY_L] = 0x26;
-    TransCodeTable_g[PS2_KEY_M] = 0x32;
-    TransCodeTable_g[PS2_KEY_N] = 0x31;
-    TransCodeTable_g[PS2_KEY_O] = 0x18;
-    TransCodeTable_g[PS2_KEY_P] = 0x19;
-    TransCodeTable_g[PS2_KEY_Q] = 0x10;
-    TransCodeTable_g[PS2_KEY_R] = 0x13;
-    TransCodeTable_g[PS2_KEY_S] = 0x1F;
-    TransCodeTable_g[PS2_KEY_T] = 0x14;
-    TransCodeTable_g[PS2_KEY_U] = 0x16;
-    TransCodeTable_g[PS2_KEY_V] = 0x2F;
-    TransCodeTable_g[PS2_KEY_W] = 0x11;
-    TransCodeTable_g[PS2_KEY_X] = 0x2D;
-    TransCodeTable_g[PS2_KEY_Y] = 0x15;
-    TransCodeTable_g[PS2_KEY_Z] = 0x2c;
-    TransCodeTable_g[PS2_KEY_EQUAL] = 0x0D;
-    TransCodeTable_g[PS2_KEY_F1] = 0x3B;
-    TransCodeTable_g[PS2_KEY_F2] = 0x3C;
-    TransCodeTable_g[PS2_KEY_F3] = 0x3D;
-    TransCodeTable_g[PS2_KEY_F4] = 0x3E;
-    TransCodeTable_g[PS2_KEY_F5] = 0x3F;
-    TransCodeTable_g[PS2_KEY_F6] = 0x40;
-    TransCodeTable_g[PS2_KEY_F7] = 0x41;
-    TransCodeTable_g[PS2_KEY_F8] = 0x42;
-    TransCodeTable_g[PS2_KEY_F9] = 0x43;
-    TransCodeTable_g[PS2_KEY_F10] = 0x44;
-    TransCodeTable_g[PS2_KEY_F11] = 0x85;
-    TransCodeTable_g[PS2_KEY_F12] = 0x86;
-}
+void setup_trans_table();
 
 /**
  * @brief Set the up XT keyboard object.
  * 
  */
-void setup_xt_kbd()
-{
-    pinMode(PIN_XT_CLK, OUTPUT);
-    pinMode(PIN_XT_DATA, OUTPUT);
-    digitalWrite(PIN_XT_CLK, HIGH);
-    digitalWrite(PIN_XT_DATA, HIGH);
-}
+void setup_xt_kbd();
 
 /**
  * @brief Send key via XT keyboard interface.
  * 
  * @param value 
  */
-void send_xt_kbd(unsigned char value)
-{
-    while (digitalRead(PIN_XT_CLK) != HIGH);
-    
-    unsigned char bits[8];
-    byte p = 0;
-    
-    for (byte index = 0; index < 8; index++)
-    {
-        if (value & 1)
-        {
-            bits[index] = 1;
-        }
-        else
-        {
-            bits[index] = 0;
-        }
+void send_xt_kbd(unsigned char value);
 
-        value = value >> 1;
-    }
-    
-    digitalWrite(PIN_XT_CLK, LOW);
-    digitalWrite(PIN_XT_DATA, HIGH);
-    delayMicroseconds(120);
-    digitalWrite(PIN_XT_CLK, HIGH);
-    delayMicroseconds(66);
-    digitalWrite(PIN_XT_CLK, LOW);
-    delayMicroseconds(30);
-    digitalWrite(PIN_XT_CLK, HIGH);
-        
-    for (byte index = 0; index < 8; index++)
-    {
-        digitalWrite(PIN_XT_CLK, HIGH);
-        digitalWrite(PIN_XT_DATA, bits[p]);
-        delayMicroseconds(95);
-        digitalWrite(PIN_XT_CLK, LOW);
-        digitalWrite(PIN_XT_DATA, LOW);
-        p++;
-    }
-    
-    digitalWrite(PIN_XT_CLK, HIGH);
-    digitalWrite(PIN_XT_DATA, LOW);
-    delay(1);
-}
+#pragma endregion
 
 /**
  * @brief Setup
@@ -370,3 +223,185 @@ void loop()
         send_xt_kbd(0xAA);
     }
 }
+
+#pragma region Functions
+
+/**
+ * @brief Set the up trans table map.
+ * 
+ */
+void setup_trans_table()
+{
+    for (int index = 0; index < TRANS_CODE_TABLE_SIZE; index++)
+    {
+        TransCodeTable_g[index] = 0xff;
+    }
+
+    TransCodeTable_g[PS2_KEY_SCROLL] = 0x46;
+    TransCodeTable_g[PS2_KEY_CAPS] = 0x3A;
+    TransCodeTable_g[PS2_KEY_PAUSE] = 0x5f;
+    TransCodeTable_g[PS2_KEY_L_SHIFT] = 0x2A;
+    TransCodeTable_g[PS2_KEY_R_SHIFT] = 0x36;
+    TransCodeTable_g[PS2_KEY_L_ALT] = 0x38;
+    TransCodeTable_g[PS2_KEY_L_CTRL] = 0x1D;
+    TransCodeTable_g[PS2_KEY_L_ARROW] = 0x4B;
+    TransCodeTable_g[PS2_KEY_R_ARROW] = 0x4D;
+    TransCodeTable_g[PS2_KEY_UP_ARROW] = 0x48;
+    TransCodeTable_g[PS2_KEY_DN_ARROW] = 0x50;
+    TransCodeTable_g[PS2_KEY_DELETE] = 0x53;
+    TransCodeTable_g[PS2_KEY_ESC] = 0x01;
+    TransCodeTable_g[PS2_KEY_BS] = 0x0E;
+    TransCodeTable_g[PS2_KEY_TAB] = 0x0F;
+    TransCodeTable_g[PS2_KEY_ENTER] = 0x1C;
+    TransCodeTable_g[PS2_KEY_SPACE] = 0x39;
+    TransCodeTable_g[PS2_KEY_SEMI] = 0x27;
+    TransCodeTable_g[PS2_KEY_KP0] = 0x52;
+    TransCodeTable_g[PS2_KEY_KP1] = 0x4F;
+    TransCodeTable_g[PS2_KEY_KP2] = 0x50;
+    TransCodeTable_g[PS2_KEY_KP3] = 0x51;
+    TransCodeTable_g[PS2_KEY_KP4] = 0x4B;
+    TransCodeTable_g[PS2_KEY_KP5] = 0x4C;
+    TransCodeTable_g[PS2_KEY_KP6] = 0x4D;
+    TransCodeTable_g[PS2_KEY_KP7] = 0x47;
+    TransCodeTable_g[PS2_KEY_KP8] = 0x48;
+    TransCodeTable_g[PS2_KEY_KP9] = 0x49;
+    TransCodeTable_g[PS2_KEY_OPEN_SQ] = 0x1A;
+    TransCodeTable_g[PS2_KEY_CLOSE_SQ] = 0x1B;
+    TransCodeTable_g[PS2_LOCK_NUM] = 0x45;
+    //TransCodeTable_g[PS2_KEY_KP_DOT]=0x53;
+    TransCodeTable_g[PS2_KEY_KP_PLUS] = 0x4E;
+    TransCodeTable_g[PS2_KEY_KP_MINUS] = 0x4A;
+    TransCodeTable_g[PS2_KEY_KP_TIMES] = 0x37;
+    TransCodeTable_g[PS2_KEY_KP_DIV] = 0x35;
+    TransCodeTable_g[PS2_KEY_KP_ENTER] = 0x01;
+    TransCodeTable_g[PS2_KEY_0] = 0x0B;
+    TransCodeTable_g[PS2_KEY_1] = 0x02;
+    TransCodeTable_g[PS2_KEY_2] = 0x03;
+    TransCodeTable_g[PS2_KEY_3] = 0x04;
+    TransCodeTable_g[PS2_KEY_4] = 0x05;
+    TransCodeTable_g[PS2_KEY_5] = 0x06;
+    TransCodeTable_g[PS2_KEY_6] = 0x07;
+    TransCodeTable_g[PS2_KEY_7] = 0x08;
+    TransCodeTable_g[PS2_KEY_8] = 0x09;
+    TransCodeTable_g[PS2_KEY_9] = 0x0A;
+    TransCodeTable_g[PS2_KEY_APOS] = 0x28;
+    TransCodeTable_g[PS2_KEY_COMMA] = 0x33;
+    TransCodeTable_g[PS2_KEY_MINUS] = 0x0C;
+    TransCodeTable_g[PS2_KEY_SCROLL] = 0x46;
+    TransCodeTable_g[PS2_KEY_DOT] = 0x34;
+    TransCodeTable_g[PS2_KEY_BACK] = 0x2B;
+    TransCodeTable_g[PS2_KEY_SINGLE] = 0x29;
+    TransCodeTable_g[PS2_KEY_DIV] = 0x35;
+    TransCodeTable_g[PS2_KEY_SINGLE] = 28;
+    TransCodeTable_g[PS2_KEY_A] = 0x1E;
+    TransCodeTable_g[PS2_KEY_B] = 0x30;
+    TransCodeTable_g[PS2_KEY_C] = 0x2E;
+    TransCodeTable_g[PS2_KEY_D] = 0x20;
+    TransCodeTable_g[PS2_KEY_E] = 0x12;
+    TransCodeTable_g[PS2_KEY_F] = 0x21;
+    TransCodeTable_g[PS2_KEY_G] = 0x22;
+    TransCodeTable_g[PS2_KEY_H] = 0x23;
+    TransCodeTable_g[PS2_KEY_I] = 0x17;
+    TransCodeTable_g[PS2_KEY_J] = 0x24;
+    TransCodeTable_g[PS2_KEY_K] = 0x25;
+    TransCodeTable_g[PS2_KEY_L] = 0x26;
+    TransCodeTable_g[PS2_KEY_M] = 0x32;
+    TransCodeTable_g[PS2_KEY_N] = 0x31;
+    TransCodeTable_g[PS2_KEY_O] = 0x18;
+    TransCodeTable_g[PS2_KEY_P] = 0x19;
+    TransCodeTable_g[PS2_KEY_Q] = 0x10;
+    TransCodeTable_g[PS2_KEY_R] = 0x13;
+    TransCodeTable_g[PS2_KEY_S] = 0x1F;
+    TransCodeTable_g[PS2_KEY_T] = 0x14;
+    TransCodeTable_g[PS2_KEY_U] = 0x16;
+    TransCodeTable_g[PS2_KEY_V] = 0x2F;
+    TransCodeTable_g[PS2_KEY_W] = 0x11;
+    TransCodeTable_g[PS2_KEY_X] = 0x2D;
+    TransCodeTable_g[PS2_KEY_Y] = 0x15;
+    TransCodeTable_g[PS2_KEY_Z] = 0x2c;
+    TransCodeTable_g[PS2_KEY_EQUAL] = 0x0D;
+    TransCodeTable_g[PS2_KEY_F1] = 0x3B;
+    TransCodeTable_g[PS2_KEY_F2] = 0x3C;
+    TransCodeTable_g[PS2_KEY_F3] = 0x3D;
+    TransCodeTable_g[PS2_KEY_F4] = 0x3E;
+    TransCodeTable_g[PS2_KEY_F5] = 0x3F;
+    TransCodeTable_g[PS2_KEY_F6] = 0x40;
+    TransCodeTable_g[PS2_KEY_F7] = 0x41;
+    TransCodeTable_g[PS2_KEY_F8] = 0x42;
+    TransCodeTable_g[PS2_KEY_F9] = 0x43;
+    TransCodeTable_g[PS2_KEY_F10] = 0x44;
+    TransCodeTable_g[PS2_KEY_F11] = 0x85;
+    TransCodeTable_g[PS2_KEY_F12] = 0x86;
+}
+
+/**
+ * @brief Set the up XT keyboard object.
+ * 
+ */
+void setup_xt_kbd()
+{
+    pinMode(PIN_XT_CLK, OUTPUT);
+    pinMode(PIN_XT_DATA, OUTPUT);
+    digitalWrite(PIN_XT_CLK, HIGH);
+    digitalWrite(PIN_XT_DATA, HIGH);
+}
+
+/**
+ * @brief Send key via XT keyboard interface.
+ * 
+ * @param value 
+ */
+void send_xt_kbd(unsigned char value)
+{
+
+#ifdef DUMMY_SEND
+    return;
+#endif
+
+    // 
+    unsigned char BitsL[XT_BYTE_SIZE];
+
+    byte PointerL = 0;
+
+    // Wait sync front.
+    while (digitalRead(PIN_XT_CLK) != HIGH);
+    
+    for (byte index = 0; index < XT_BYTE_SIZE; index++)
+    {
+        if (value & 1)
+        {
+            BitsL[index] = HIGH;
+        }
+        else
+        {
+            BitsL[index] = LOW;
+        }
+
+        value = value >> 1;
+    }
+    
+    digitalWrite(PIN_XT_CLK, LOW);
+    digitalWrite(PIN_XT_DATA, HIGH);
+    delayMicroseconds(120);
+    digitalWrite(PIN_XT_CLK, HIGH);
+    delayMicroseconds(66);
+    digitalWrite(PIN_XT_CLK, LOW);
+    delayMicroseconds(30);
+    digitalWrite(PIN_XT_CLK, HIGH);
+        
+    for (byte index = 0; index < XT_BYTE_SIZE; index++)
+    {
+        digitalWrite(PIN_XT_CLK, HIGH);
+        digitalWrite(PIN_XT_DATA, BitsL[PointerL]);
+        delayMicroseconds(95);
+        digitalWrite(PIN_XT_CLK, LOW);
+        digitalWrite(PIN_XT_DATA, LOW);
+        PointerL++;
+    }
+    
+    digitalWrite(PIN_XT_CLK, HIGH);
+    digitalWrite(PIN_XT_DATA, LOW);
+    delay(1);
+}
+
+#pragma endregion
